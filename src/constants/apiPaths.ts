46b004d8d2c38ef0a118e1dfdc6c6b3c737297c3
Serhii_Yudin <serhii.yudin@regeneron.com>
2024-11-07 23:43:25 +0200

    task-5 finished

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://fa-imports-service-yudin-ne-001.azurewebsites.net/api",
+  bff: "https://fa-products-service-yudin-001.azurewebsites.net/api",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
